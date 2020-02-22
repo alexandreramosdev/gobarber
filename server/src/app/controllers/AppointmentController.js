1a879f1e43a3ea42b19bb14bd4be28fc934d50eb
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-22 20:47:45 -0300

    create jobs

--- server/src/app/controllers/AppointmentController.js
+++ server/src/app/controllers/AppointmentController.js
@@ -2,7 +2,8 @@ import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -152,17 +153,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', ás' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
