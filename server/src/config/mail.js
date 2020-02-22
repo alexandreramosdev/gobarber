cb032ac9d6ac4c5c280d84fe23819431bde10f45
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-22 20:03:39 -0300

    create send mail

---
+++ server/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c5d2efa569a6a1',
+    pass: 'dc8e1863473aaf',
+  },
+  default: {
+    from: 'Equipe Vulgocode <noreply@vulgocode.com>',
+  },
+};
