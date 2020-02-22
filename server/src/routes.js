152990dfe1aa6b6f8cdccda0f27fce11bda8cec1
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-22 15:21:55 -0300

    create appointments

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,8 +5,9 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/authMiddleware';
 
@@ -21,6 +22,9 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
