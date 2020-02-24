ca6057714ccda005eb08aeddbdfbaf6879ead413
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-24 16:35:27 -0300

    add prettier and lint

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
-import Profile from "../pages/Profile";
-import Dashboard from "../pages/Dashboard";
+import Profile from '../pages/Profile';
+import Dashboard from '../pages/Dashboard';
 
 export default function Routes() {
   return (
