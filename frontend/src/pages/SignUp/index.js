8cbcc36aa6d1f0cc27630c6a141cd609c87663b2
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-24 19:15:36 -0300

    style Signup an SignIn and add validation form

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.svg';
+import Input from '~/components/form/Input';
 
 export default function SignUp() {
-  return <div />;
+  const formRef = useRef(null);
+
+  const handleSubmit = async data => {
+    try {
+      // Remove all previous errors
+      formRef.current.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string()
+          .email('Insira um e-mail válido')
+          .required('O email é obrigatório'),
+        password: Yup.string()
+          .min(6, 'No mínimo 6 caracteres')
+          .required('A senha é obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // Validation passed
+      console.tron.log(data);
+    } catch (err) {
+      const validationErrors = {};
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach(error => {
+          validationErrors[error.path] = error.message;
+        });
+        formRef.current.setErrors(validationErrors);
+      }
+    }
+  };
+
+  return (
+    <>
+      <img src={logo} alt="gobarber" />
+
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
