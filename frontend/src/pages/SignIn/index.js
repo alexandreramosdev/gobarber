e2267aa58844800d9d9a8dfee42385d3fdc06b17
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-24 21:19:54 -0300

    add signin redux

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,18 +1,23 @@
 import React, { useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/images/logo.svg';
 import Input from '~/components/form/Input';
 
 export default function SignIn() {
   const formRef = useRef(null);
+  const dispatch = useDispatch();
 
   const handleSubmit = async data => {
     try {
       // Remove all previous errors
       formRef.current.setErrors({});
+
       const schema = Yup.object().shape({
         email: Yup.string()
           .email('Insira um e-mail válido')
@@ -26,8 +31,9 @@ export default function SignIn() {
         abortEarly: false,
       });
 
-      // Validation passed
-      console.tron.log(data);
+      const { email, password } = data;
+
+      dispatch(signInRequest(email, password));
     } catch (err) {
       const validationErrors = {};
       if (err instanceof Yup.ValidationError) {
--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,3 +1,23 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload: { email, password } }) {
+  const res = yield call(api.post, '/sessions', { email, password });
+
+  const { token, user } = res.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuário não é prestador');
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
