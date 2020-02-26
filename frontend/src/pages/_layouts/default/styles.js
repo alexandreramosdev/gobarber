b0348bbb4b267d1b835030ff178bce08a4e075d2
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-26 02:30:50 -0300

    feat list schedule

--- frontend/src/pages/_layouts/default/styles.js
+++ frontend/src/pages/_layouts/default/styles.js
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #663399, #ab59c1);
+  padding-bottom: 10px;
+  min-height: 100vh;
 `;
