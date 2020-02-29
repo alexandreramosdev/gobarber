67afd5e0b6655e31a20e900c9f1009cdba2ea27d
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-29 02:06:19 -0300

    add app

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
 
-// import { Container } from './styles';
+import '~/config/Reactotron';
+
+import { store, persistor } from './store';
+import Routes from './routes';
 
 export default function App() {
-  return <Text />;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#663399" />
+        <Routes />
+      </PersistGate>
+    </Provider>
+  );
 }
